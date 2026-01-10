81c4d51d28e4afcd24ffabfd70bb4a08e358f4ea
BaselCloudSec <baselkatabe8.2011@gmail.com>
2026-01-10 01:06:04 +0100

    Create automation_logic_script.py

---
+++ automation_logic_script.py
@@ -0,0 +1,7 @@
+cars = ["bmw", "mercides", "audi"]
+adjectives = ["fast", "expensive"]
+   for car in cars :
+        print(car + ":")
+   for adj in adjectives :
+        print("-" + adj)
+   print("\n")
