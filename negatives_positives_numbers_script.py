58590065abf7d1998ba60c8af6789b192ecfef03
BaselCloudSec <baselkatabe8.2011@gmail.com>
2026-01-10 12:44:08 +0100

    Create negatives_positives_numbers_script.py

---
+++ negatives_positives_numbers_script.py
@@ -0,0 +1,10 @@
+number = input("choose number : ")
+number = int(number)
+
+if number >= 0 :
+    if number == 0 :
+        print("zero")
+    else :
+     print("positive number")
+else :
+    print("negative number")
