22252c2c517c75dcb836e94e0bcf22914ee67bb6
BaselCloudSec <baselkatabe8.2011@gmail.com>
2026-01-11 12:44:01 +0100

    Create dictionaries_keys_values_script.py

---
+++ dictionaries_keys_values_script.py
@@ -0,0 +1,24 @@
+
+ # Dictionaries
+ # Keys
+ # Values
+ # Keys : values
+                #key : value
+student_age = {"ali" : 17,
+                "basel" : 19,
+                "tim" : 19, }
+print(student_age.get("basel"))
+
+months = {1 : "January",
+          2 : "February",
+          3 : "March",
+          4 : "April",
+          5 : "May",
+          6 : "June",
+          7 : "July",
+          8 : "August",
+          9 : "September",
+          10 : "October",
+          11 : "November",
+          12 : "December"}
+print(months.get(7))
