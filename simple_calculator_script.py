e21cb4b92ecc24b09a80b06da65ec07fdb872360
BaselCloudSec <baselkatabe8.2011@gmail.com>
2026-01-12 16:38:24 +0100

    Create simple_calculator_script.py

---
+++ simple_calculator_script.py
@@ -0,0 +1,11 @@
+number1 = int(input("Enter a number : "))
+number2 = int(input("Enter the another number : "))
+xxbb = input("choose what to do : + or - or * or / : ")
+if xxbb == "+":
+    print(int(number1) + int(number2))
+if xxbb == "-":
+      print(number1 - number2)
+if xxbb == "*":
+      print(number1 * number2)
+if xxbb == "/":
+    print(number1 / number2)
